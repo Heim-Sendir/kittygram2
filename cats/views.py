f7fbfac3a238306be98bfa7fe060edc2967ee189
Heim_Sendir <alex.noa29@gamil.com>
2025-01-10 15:25:05 +0500

    fix api

--- cats/views.py
+++ cats/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
 
 from .models import Achievement, Cat, User
 
@@ -8,6 +11,18 @@ from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({"message": "Объект успешно удалён!"},
+                        status=status.HTTP_204_NO_CONTENT)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
